3a572f2bff95fee868a35583a4ba42a1e9b441e6
Liam Nunns <liam.nunns@verrency.com>
2019-11-24 12:53:48 +1100

    catering for inability to buy and sell in a single indice

--- stocks.py
+++ stocks.py
@@ -1,12 +1,10 @@
 def calculateprofit(price_array):
-    profit = 0
-    buyprice = 0
-    sellprice = 0
     length = len(price_array)
     i = 0
+    profit = 0
 
-    while i < length:
-        maximum = max(price_array[i:])
+    while i + 1 < length:
+        maximum = max(price_array[1+i:])
         if maximum - price_array[i] > profit:
             profit = maximum - price_array[i]
             buyprice = price_array[i]
